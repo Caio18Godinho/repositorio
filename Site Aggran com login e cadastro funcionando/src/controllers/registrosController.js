7f0a3f2c6e475f0bf1d59479668581e7a824bca6
Caio18Godinho <caio.mota@sptech.school>
2025-12-03 21:47:16 -0300

    Erick2

--- Site Aggran com login e cadastro funcionando/src/controllers/registrosController.js
+++ Site Aggran com login e cadastro funcionando/src/controllers/registrosController.js
@@ -2,12 +2,9 @@ var registrosModel = require("../models/registrosModel");
 
 function buscarUltimosRegistros(req, res) {
 
-    var Hectare_ID = req.params.Hectare_ID
-    var ID_USUARIO = registrosModel.params.
+    var ID_USUARIO = req.params.ID_USUARIO;
 
-    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
-
-    registrosModel.buscarUltimosRegistros(Hectare_ID, ID_USUARIO).then(function (resultado) {
+    registrosModel.buscarUltimosRegistros(ID_USUARIO).then(function (resultado) {
         if (resultado.length > 0) {
             res.status(200).json(resultado);
         } else {
@@ -19,8 +16,18 @@ function buscarUltimosRegistros(req, res) {
         res.status(500).json(erro.sqlMessage);
     });
 };
+function listar(req, res) {
 
+    registrosModel.listar()
+    .then(function (resultado) {
+        console.log('controller listar', resultado)
+        // precisamos informar que o resultado voltará para o front-end como uma resposta em json
+        res.status(200).json(resultado);
+    }).catch(function (erro) {
+        res.status(500).json(erro);
+    })
+}
 module.exports = {
-    buscarUltimosRegistros
-
+    buscarUltimosRegistros,
+    listar
 }
--- Site Aggran com login e cadastro funcionando/src/routes/registros.js
+++ Site Aggran com login e cadastro funcionando/src/routes/registros.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var router = express.Router();
-
 var registrosController = require("../controllers/registrosController");
 
-router.get("/registros/:Hectare_ID:ID_USUARIO", function (req, res) {
+
+router.get("/listar", function (req, res) {
+    registrosController.listar(req, res);
+});
+
+router.get("/buscarUltimosRegistros", function (req, res) {
     registrosController.buscarUltimosRegistros(req, res);
 });
 
